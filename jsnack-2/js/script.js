91b051193c11d68f66085b902774d1705e718c17
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-03-22 16:11:43 +0100

    jsnack2 completato

--- jsnack-2/js/script.js
+++ jsnack-2/js/script.js
@@ -1,76 +1,104 @@
 console.log('JS OK!');
 
-// ----- JSNACK 1 ----- //
-//Crea un array di 10 oggetti che rappresentano una zucchina
-//indicando per ognuna varietà, peso e lunghezza
-//Calcola quanto pesano tutte le zucchine
+// ----- JSNACK 2 ----- //
+//Crea 10 oggetti che rappresentano una zucchina
+//Dividi in due array le zucchine che misurano meno o più di 15cm
+//Infine stampa separatamente quanto pesano i due gruppi di zucchine
 
+
+// ----- FUNCTIONS ----- //
 function randomWeight() {
     return Math.floor(Math.random() * 450) + 50;
 }
 
-function totalWeight() {
+function randomLength() {
+    return Math.floor(Math.random() * 20) + 10;
+}
+
+function totalWeight(array) {
     let sum = 0;
-    for (let i = 0; i < zucchine.length; i++) {
-        sum += zucchine[i].weight;
-        console.log(zucchine[i].weight);
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i].weight;
+        //console.log(zucchine[i].weight);
     }
     return sum;
 }
 
+function verifiedLength() {
+
+    for (let i = 0; i < zucchine.length; i++) {
+
+        if (zucchine[i].length < 15) {
+            shortZucchine.push(zucchine[i]);
+        } else {
+            longZucchine.push(zucchine[i]);
+        }
+    }
+}
+
+
+// ----- GENERAL ----- //
 const zucchine = [{
     variety: 'nera',
     weight: randomWeight(),
-    length: '26cm'
+    length: randomLength()
 },
 {
     variety: 'tonde',
     weight: randomWeight(),
-    length: '26cm'
+    length: randomLength()
 },
 {
     variety: 'trombetta',
     weight: randomWeight(),
-    length: '26cm'
+    length: randomLength()
 },
 {
     variety: 'pâtisson',
     weight: randomWeight(),
-    length: '26cm'
+    length: randomLength()
 },
 {
     variety: 'Crookneck',
     weight: randomWeight(),
-    length: '26cm'
+    length: randomLength()
 },
 {
     variety: 'giallo',
     weight: randomWeight(),
-    length: '26cm'
+    length: randomLength()
 },
 {
     variety: 'napoletana',
     weight: randomWeight(),
-    length: '26cm'
+    length: randomLength()
 },
 {
     variety: 'fiorentino',
     weight: randomWeight(),
-    length: '26cm'
+    length: randomLength()
 },
 {
     variety: 'romanesco',
     weight: randomWeight(),
-    length: '26cm'
+    length: randomLength()
 },
 {
     variety: 'striata',
     weight: randomWeight(),
-    length: '26cm'
+    length: randomLength()
 },
 ];
 
-console.log('Array lungo: ' + zucchine.length);
+//array lunghezze zucchine
+const shortZucchine = [];
+const longZucchine = [];
 
-totalWeight();
-console.log('Il peso totale delle zucchine è: ' + totalWeight());
+
+totalWeight(zucchine);
+verifiedLength()
+console.log('Le zucchine più corte di 15cm: ', shortZucchine);
+console.log('Le zucchine più lunghe di 15cm: ', longZucchine);
+console.log('Il peso totale delle zucchine è: ', totalWeight(zucchine));
+console.log('Il peso delle zucchine inferiori ai 15cm è: ', totalWeight(shortZucchine));
+console.log('Il peso delle zucchine superiori ai 15cm è: ', totalWeight(longZucchine));
